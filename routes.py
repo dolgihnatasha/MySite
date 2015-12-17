4374185475d96b8c4e3693beb4fe0915291eb798
Dolgih Natalia <n.e.dolgih@yandex.ru>
2015-12-17 22:51:22 +0500

    fix

--- routes.py
+++ routes.py
@@ -9,6 +9,7 @@ import json
 myphoto = 'https://avatars0.githubusercontent.com/u/6077501?v=3&s=460'
 visit = 0
 
+
 def write_visits(data):
     with open('data.json', 'w') as d:
         json.dump(data, d)
@@ -19,16 +20,25 @@ def read_visits():
         data = json.load(d)
     return data
 
+
+def add_visits():
+    data = read_visits()
+    data['visit'] += 1
+    if data['unique']:
+        data['unique'] = set()
+    data['unique'].add(request.get('REMOTE_ADDR'))
+    print(request.get('REMOTE_ADDR'))
+    write_visits(data)
+    return data
+
+
 def check_visit():
     try:
         cookie = request.get_cookie("visit")
 
     except (NameError, TypeError):
         response.set_cookie("visit", str(True))
-    data = read_visits()
-    data['visit'] += 1
-    visit = data['visit']
-    write_visits(data)
+        add_visits()
 
 
 
@@ -37,7 +47,7 @@ def check_visit():
 @view('index')
 def home():
     """Renders the home page."""
-    check_visit()
+    v = check_visit()
     with open('aboutme.txt') as f:
         info = f.read()
     return dict(
@@ -46,21 +56,21 @@ def home():
         scripts='',
         myPhoto=myphoto,
         aboutMe=info,
-        visits=visit
+        visits=v
     )
 
 @route('/gallery')
 @view('gallery')
 def gallery():
     """Renders the contact page."""
-    check_visit()
+    v = check_visit()
     pics = [f for x in os.walk('./static/RV/small/') for f in x[2]]
     return dict(
         title='Gallery',
         stylesheet='gallery.css',
         scripts='<script>',
         pictures=pics,
-        visits=visit
+        visits=v
     )
 
 @route('/projects')
@@ -68,9 +78,9 @@ def gallery():
 @view('projects')
 def projects():
     """Renders the project page."""
-    check_visit()
+    v = check_visit()
     return dict(
         title='Projects',
         stylesheet='index2.css',
-        visits=visit
+        visits=v
     )
